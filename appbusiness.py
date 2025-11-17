a3a4f977d0cd02714dc4159e494b70275c337862
JYCODER231 <jchen123cjy@gmail.com>
2025-11-17 14:50:22 +0800

    Update appbusiness.py

--- appbusiness.py
+++ appbusiness.py
@@ -1,1 +1,12 @@
+import streamlit as st
+
+st.write("Retail business dashboard")
+
+st.header("Header")
+st.write("MESSAGE")
+
+age = st.number_input("Enter Monyhly sales target (in usd):",
+                      min_value=0,
+                      max_value=100000,
+                      value=50000)
 
