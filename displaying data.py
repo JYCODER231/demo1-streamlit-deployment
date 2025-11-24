f7d71a557825bc47e94c7be087ddf3ec9c5fb2d3
JYCODER231 <jchen123cjy@gmail.com>
2025-11-24 14:23:27 +0800

    Update displaying data.py

--- displaying data.py
+++ displaying data.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import pandas as pd
-import os
+import os 
 
 # Get the current working directory
-current_directory = os.getcwd()
+current_directory = os.getcwd() #ensure locate the csv file
 # Define the file path
-file_path = os.path.join(current_directory, 'winequality-red.csv')
+file_path = os.path.join(current_directory, 'winequality-red.csv') #can use full path locate the csv file
 
 # Read the CSV file into a DataFrame
-df = pd.read_csv(file_path, delimiter=';')
+df = pd.read_csv(file_path, delimiter=';') #df stands for dataframe
 
 # Display the DataFrame in an interactive table
 st.write("Wine Quality Data")
-st.dataframe(df)
+st.dataframe(df)#when using the dataframe function the little buttoms will show at the right corner.
